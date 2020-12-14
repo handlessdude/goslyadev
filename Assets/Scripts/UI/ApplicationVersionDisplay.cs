48ef3081112cfda4166a3db57856ecb8a9f2eab9
elia-morrison <andsimof@gmail.com>
2020-12-14 11:03:20 +0300

    version label added

---
+++ Assets/Scripts/UI/ApplicationVersionDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ApplicationVersionDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    private void Start()
+    {
+        if (!text)
+        {
+            text = GetComponent<TextMeshProUGUI>();
+        }
+
+        text.text = Application.version;
+    }
+}
