40c6b471c0cdfdc9bcb822ab755bbbdcd8619840
inrag3 <70843287+inrag3@users.noreply.github.com>
2021-04-07 16:20:41 +0300

    added new dialogs v2.0

---
+++ Assets/Scripts/DialogueSystem/ThiefRobot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThiefRobot : DialogueNPC
+{
+    void Start()
+    {
+        selfBubbleHeight = 2.3f;
+        currentDialogElement = new SequentialDialogueElement("ask_rob_bout_board", false,
+            new SequentialDialogueElement("ask_rob_bout_board2",false,
+            new SequentialDialogueElement("robot_panic", false, new SequentialDialogueElement("k_demands_plate", true,
+            new SequentialDialogueElement("rob_gives_board", false, new DialogueEndElement()))
+            )));
+    }
+
+
+    protected override void DialogueExit()
+    {
+        base.DialogueExit();
+    }
+
+
+    public override void Response(int i)
+    {
+        base.Response(i);
+        Invoke("RestoreIdle", 2.0f);
+    }
+
+
+    void RestoreIdle()
+    {
+        CancelInvoke("RestoreIdle");
+    }
+}
