7a9031076258d37bccec9be0aba9185958ad8313
inrag3 <70843287+inrag3@users.noreply.github.com>
2021-05-06 01:06:58 +0300

    added stomprange with @vladdeg96
    Co-Authored-By: vladdeg96 <69649512+vladdeg96@users.noreply.github.com>

---
+++ Assets/Scripts/Player/StompRange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StompRange : MonoBehaviour
+{
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!collision.isTrigger)
+            PlayerController.isStopmAllowed = false;   
+    }
+
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.isTrigger)
+            PlayerController.isStopmAllowed = true; 
+    }
+}
