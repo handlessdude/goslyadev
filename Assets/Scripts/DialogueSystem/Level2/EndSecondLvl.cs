1cb394d6a8120de3840ee66f0c1405974c81fed9
vladdeg96 <69649512+vladdeg96@users.noreply.github.com>
2021-05-10 19:55:40 +0300

    changed some dialogs

--- Assets/Scripts/DialogueSystem/Level2/EndSecondLvl.cs
+++ Assets/Scripts/DialogueSystem/Level2/EndSecondLvl.cs
@@ -17,10 +17,11 @@ public class EndSecondLvl : DialogueNPC
                 new SequentialDialogueElement("meets_hosp", true,
                 new SequentialDialogueElement("news_bout_son1", true,
                 new SequentialDialogueElement("news_bout_son2", true,
+                new SequentialDialogueElement("news_bout_son3", true,
                 new SequentialDialogueElement("hung_up", false,
                 new SequentialDialogueElement("have_no_time1", true,
                 new SequentialDialogueElement("have_no_time2", true,
-                new DialogueEndElement()))))))))));
+                new DialogueEndElement())))))))))));
     }
 
     protected override void DialogueExit()
