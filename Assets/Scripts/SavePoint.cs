9227a2dc76919c209c6c8b34fcee1474bbb48aaf
inrag3 <70843287+inrag3@users.noreply.github.com>
2021-05-16 01:43:12 +0300

    preparation 3 lvl
    Co-Authored-By: elia-morrison <54751074+elia-morrison@users.noreply.github.com>
    Co-Authored-By: handlessdude <61969421+handlessdude@users.noreply.github.com>
    Co-Authored-By: vladdeg96 <69649512+vladdeg96@users.noreply.github.com>

---
+++ Assets/Scripts/SavePoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePoint : SaveSystem
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SaveGame();
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
